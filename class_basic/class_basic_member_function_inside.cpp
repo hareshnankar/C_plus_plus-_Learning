f31221f1a5ad2310364612b81ccfb8744f688d66
hareshnankar <hareshnankar1234@GMAIL.COM>
2024-11-16 19:07:01 +0530

    basics of class

---
+++ class_basic/class_basic_member_function_inside.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring> // For strcpy
+/*declrae_member function inside class*/
+
+class student
+{
+    int id;
+    int roll_no;
+    char name[10];
+
+public:
+    void set_data(int a, int b, const char c[20])
+    {
+        id = a;
+        roll_no = b;
+        std::strcpy(name, c); // Use strcpy to copy the string
+    }
+
+    void get_data() const
+    {
+        std::cout << "ID: " << id << std::endl;
+        std::cout << "Roll No: " << roll_no << std::endl;
+        std::cout << "Name: " << name << std::endl;
+    }
+};
+
+int main()
+{
+    student s1; // Create an object of student
+    s1.set_data(1, 101, "John"); // Set data for the student
+    s1.get_data(); // Get and print the data
+
+    return 0;
+}
---
+++ class_basic/class_basic_member_function_outside.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring> // For strcpy
+/*declrae_member function outside class*/
+
+class student
+{
+    int id;
+    int roll_no;
+    char name[10];
+
+public:
+    void set_data(int a, int b, const char c[20]);
+    void get_data();
+
+};
+void student :: set_data(int a, int b, const char c[20])
+{
+    id = a;
+    roll_no = b;
+    std::strcpy(name, c); // Use strcpy to copy the string
+}
+void student :: get_data(void)
+{
+    std::cout << "ID: " << id << std::endl;
+    std::cout << "Roll No: " << roll_no << std::endl;
+    std::cout << "Name: " << name << std::endl;
+}
+int main()
+{
+    student s1; // Create an object of student
+    s1.set_data(1, 101, "John"); // Set data for the student
+    s1.get_data(); // Get and print the data
+
+    return 0;
+}
