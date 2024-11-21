0f879c17fc6045748a34f56589c730fba47ef3bd
hareshnankar <hareshnankar1234@GMAIL.COM>
2024-11-21 16:17:01 +0530

    shallow copy

---
+++ constructor/copy_constructor_shallow_copy.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstring> // For strcpy
+using namespace std;
+
+class student
+{
+public:
+    int id = 10;
+    int *roll_no_prt = new int(4);
+
+public:
+
+    void get_data() const
+    {
+        std::cout << "ID: " << id << std::endl;
+        std::cout << "Roll No pointer containt: " << *roll_no_prt << std::endl;
+    }
+};
+
+int main()
+{
+
+    student s1;
+    cout<<"containt of S1 object....."<<endl;
+    s1.get_data(); // Get and print the data
+    s1.id = 20;
+    *(s1.roll_no_prt) = 200;
+    cout<<"containt of S2 object....."<<endl;
+    /*here you can see that we are using copy constructor and we are not provided explicit defination of copy constructor 
+    so compiler will provide implicit copy construcor abd important part here if compiler used implicit copy constructor 
+    that time shallow copy is used made to store the copied containt*/
+    /*important points while using shaallow copy/impicit copy constructor
+    1.when we use norman variable then its okay to use this type
+    2, we we use pointer variable that time issue will happen 
+    issue with pinter 
+
+ Initial State (s1)   
++-------------------+
+| id: 10            |
+| roll_no_prt: ---->| 4                     |
++-------------------+
+After Modifying s1
++-------------------+
+| id: 20            |
+| roll_no_prt: ---->| 200                   |
++-------------------+
+After Shallow Copy (s2(s1))
+
+s1
++-------------------+
+| id: 20            |
+| roll_no_prt: ---->| 200                   |
++-------------------+
+
+s2
++-------------------+
+| id: 20            |
+| roll_no_prt: ---->| 200                   |
++-------------------+
+After Modifying s2
+
++-------------------+
+| id: 20            |
+| roll_no_prt: ---->| 300                   |
++-------------------+
+
+s2
++-------------------+
+| id: 30            |
+| roll_no_prt: ---->| 300                   |
++-------------------+
+
+After Deleting s1.roll_no_prt
+
++-------------------+
+| id: 20            |
+| roll_no_prt: ---->| (deleted memory)      |
++-------------------+
+
+s2
++-------------------+
+| id: 30            |
+| roll_no_prt: ---->| (dangling pointer)    |
++-------------------+
+issue due to shallow copy, pointer will  same memory even oject is differant*/
+
+	student s2(s1);
+    s2.get_data(); // Get and print the data
+
+    return 0;
+}
